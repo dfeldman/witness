81605b5135731408c85152e663432affca2a7b84
cole kennedy <colek42@gmail.com>
2021-12-13 17:27:09 -0600

    update

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -x
 
-go build witness
+go build -o bin/witness
 openssl genpkey -algorithm ed25519 -outform PEM -out testkey.pem
 openssl pkey -in testkey.pem -pubout > testpub.pem
 
-witness sign -k testkey.pem policy.json > policy.signed.json
-witness run -s build -r https://log.testifysec.io -k testkey.pem --trace -o attestation1.json -- go build
+./bin/witness sign -k testkey.pem policy.json > policy.signed.json
+./bin/witness run -s build -r https://log.testifysec.io -k testkey.pem -o attestation1.json -- go build
 
-./witness verify -k testpub.pem -a attestation1.json -p policy.signed.json -f witness #local test
-./witness verify -k testpub.pem -r https://log.testifysec.io -p policy.signed.json -f witness #remote test
+./bin/witness verify -k testpub.pem -a attestation1.json -p policy.signed.json -f witness #local test
+./bin/witness verify -k testpub.pem -r https://log.testifysec.io -p policy.signed.json -f witness #remote test
